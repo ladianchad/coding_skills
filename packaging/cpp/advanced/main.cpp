e48df912200e4817e39c4d2f3db6eb47c53384de
ladianchad <qhrejddlvltm@gmail.com>
2024-01-24 18:24:06 +0900

    add advancded cpp example
    Signed-off-by: ladianchad <qhrejddlvltm@gmail.com>

---
+++ packaging/cpp/advanced/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <magic_enum.hpp>
+#include <glog/logging.h>
+
+
+int main(int argc, char const * argv[])
+{
+  google::InitGoogleLogging(argv[0]);
+  enum Test {
+    ONE =1 ,
+    TWO = 2,
+    TREE = 3
+  };
+  Test test = Test::TREE;
+  LOG(INFO)<<"dpkg cpp example.";
+  LOG(WARNING)<<"test enum name : "<< magic_enum::enum_name(test);
+  return 0;
+}
